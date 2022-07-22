2f5861fed66fd75e10b2437854b0017cc0489f72
opossum <ryabkoff@rambler.ru>
2022-07-22 21:36:18 +0500

    2.4. HabrCareerParse [#285213 #324744]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -16,21 +16,21 @@ import java.util.List;
 public class HabrCareerParse implements Parse {
     private final DateTimeParser dateTimeParser;
 
-    private static final String PAGE_LINK = "/vacancies/java_developer";
+    private static final int PAGE_COUNT = 5;
 
     public HabrCareerParse(DateTimeParser dateTimeParser) {
         this.dateTimeParser = dateTimeParser;
     }
 
     private String retrieveDescription(String link) {
-        String rsl = "";
+        String rsl;
         Connection connection = Jsoup.connect(link);
         Document document;
         try {
             document = connection.get();
             rsl = document.select(".collapsible-description__content").text();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new IllegalArgumentException();
         }
         return rsl;
     }
@@ -45,9 +45,10 @@ public class HabrCareerParse implements Parse {
     @Override
     public List<Post> list(String link) {
         List<Post> rsl = new ArrayList<>();
-        for (int page = 1; page <= 5; page++) {
+        for (int page = 1; page <= PAGE_COUNT; page++) {
             try {
-                Connection connection = Jsoup.connect(String.format("%s%s?page=%d", link, PAGE_LINK, page));
+                String pageLink = String.format(link, page);
+                Connection connection = Jsoup.connect(pageLink);
                 Document document = connection.get();
                 Elements rows = document.select(".vacancy-card__inner");
                 if (rows.size() == 0) {
@@ -55,7 +56,7 @@ public class HabrCareerParse implements Parse {
                 }
                 rows.forEach(row -> {
                     Element titleElement = row.select(".vacancy-card__title").first();
-                    String vacancyLink = String.format("%s%s", link,
+                    String vacancyLink = String.format("%s%s", pageLink,
                             titleElement.child(0).attr("href"));
                     rsl.add(new Post(titleElement.text(),
                             vacancyLink,
@@ -63,7 +64,7 @@ public class HabrCareerParse implements Parse {
                             retrieveDate(row)));
                 });
             } catch (IOException e) {
-                e.printStackTrace();
+                throw new IllegalArgumentException();
             }
         }
         return rsl;
@@ -71,7 +72,7 @@ public class HabrCareerParse implements Parse {
 
     public static void main(String[] args) {
         HabrCareerParse hcp = new HabrCareerParse(new HabrCareerDateTimeParser());
-        List<Post> list = hcp.list("https://career.habr.com");
+        List<Post> list = hcp.list("https://career.habr.com/vacancies/java_developer?page=%d");
         list.forEach(System.out::println);
     }
 }
